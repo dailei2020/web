b6b1384fe2777fc59c657a5759a83a8302a59a59
戴磊 <982238845@qq.com>
2020-05-06 16:24:01 +0800

    123

--- src/main/java/com/example/user/UserRepository.java
+++ src/main/java/com/example/user/UserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
     User findByUsername(String username);
-    void deleteAllByUsername(String username);
+    void deleteByUsername(String username);
 }
