dcd47087fae7979efaea23fdb92835f0b36e2803
戴磊 <982238845@qq.com>
2020-05-12 11:05:44 +0800

    123

--- src/main/java/com/example/user/UserController.java
+++ src/main/java/com/example/user/UserController.java
@@ -17,19 +17,23 @@ public class UserController {
 
     @RequestMapping("/zhuce")
     String zhuce(User user) {
-        if (user.username != null && user.password != null){
-         userRepository.save(user);
-        return "注册成功";}
-        else{
+        if (user.username != null && user.password != null) {
+            userRepository.save(user);
+            return "注册成功";
+        } else {
             return "请输入正确的用户名密码";
         }
-        @RequestMapping("/login")
-            String login(User user){
-        Iterable<User> users = userRepository.findAll();
-        for (User o : users)
-            if (o.username.equals(o.username))
-                if (o.password.equals(o.password))
-                    return "登录成功";
+    }
 
+    @RequestMapping("/login")
+    String login(User user) {
+        Iterable<User> users = userRepository.findAll();
+        for (User o : users) {
+
+            if (user.username.equals(o.username))
+                if (user.password.equals(o.password))
+                    return "登录成功";
+        }
+        return null;
     }
 }
